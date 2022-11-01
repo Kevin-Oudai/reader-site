2fa662ac0a3fec867967a8b9b10b7967e1ed52d8
Kevin Oudai <46614071+Kevin-Oudai@users.noreply.github.com>
2022-11-01 16:25:25 -0400

    error fix

--- readersite/readersite/urls.py
+++ readersite/readersite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import views
+from .views import home
 
 urlpatterns = [
     path('', views.home, name='home'),
