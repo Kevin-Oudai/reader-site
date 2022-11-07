b6cb9ceb790c826314e5932382908f864caf9497
Kevin Oudai <46614071+Kevin-Oudai@users.noreply.github.com>
2022-11-07 19:26:17 -0400

    create models

--- readersite/novelExtractor/models.py
+++ readersite/novelExtractor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Novel(models.Model):
+    titleName = models.CharField(max_length=300)
+
+class Chapters(models.Model):
+    chapterName = models.ForeignKey(Novel, on_delete=models.CASCADE)
+    url = models.URLField()
+    nextUrl = models.URLField()
+    prevUrl = models.URLField()
+    content = models.TextField()
+    
